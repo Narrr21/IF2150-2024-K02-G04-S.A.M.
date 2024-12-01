3d837f4e0a2084360220f771633ce3a2210a8e76
Henshou <gerrardgrs@gmail.com>
2024-12-01 17:37:14 +0700

    feat: entity Gudang

---
+++ backend/gudang/gudangManager.py
@@ -0,0 +1,13 @@
+from typing import List,Optional
+
+class Gudang:
+    def __init__(self, nama_gudang: str, capacity: int,max_capacity: int):
+        '''
+        Initialize a Gudang entity.
+        @param nama_gudang: Nama dari gudang.
+        @param capacity: Kapasitas gudang saat ini.
+        @param max_capacity: Kapasitas maksimum dari gudang.
+        '''
+        self.nama_gudang = nama_gudang
+        self.capacity = capacity
+        self.max_capacity = max_capacity
