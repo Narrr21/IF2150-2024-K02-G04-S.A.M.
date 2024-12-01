2e8b5da6a9a57d965744840e0e34fbae24678b3b
Julius <juliusarthur2005@gmail.com>
2024-12-01 17:41:53 +0700

    initial commit

---
+++ backend/barang/barangManager.py
@@ -0,0 +1,6 @@
+class Barang:
+    def __init__(self, name: str, capacity:int , quantitiy:int, description: str) :
+        self.name = name
+        self.capacity = capacity
+        self.quantity = quantitiy
+        self.description = description
