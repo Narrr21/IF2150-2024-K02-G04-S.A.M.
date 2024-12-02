b71479b5a0592b17a80faab19cf04a3e112a81e7
pixelatedbus <luthfihakimyusra@gmail.com>
2024-12-02 16:28:22 +0700

    fix: db

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,6 @@ def barang_from_mongo(json) -> Barang:
     barang = Barang(
         name=json["name"],
         capacity=json["capacity"],
-        quantity=json["quantity"],
         description=json["description"],
         gudang=json["gudang"]
     )
@@ -41,7 +40,7 @@ def riwayat_from_mongo(json) -> Riwayat:
 
 # CRUD for Barang
 # CREATE NEW BARANG
-def create_barang(barang: Barang, gudang: Gudang, jumlah: int) -> int:
+def create_barang(barang: Barang, gudang: Gudang, qty: int) -> int:
     
     highest_barang = barang_collection.find_one({}, sort=[("_id", -1)])
 
@@ -50,24 +49,24 @@ def create_barang(barang: Barang, gudang: Gudang, jumlah: int) -> int:
     else:
         barang._id = 1
 
-    if gudang.capacity + barang.capacity * jumlah > gudang.max_capacity:
+    if gudang.capacity + barang.capacity * qty > gudang.max_capacity:
         print("Gudang capacity exceeded")
         return -1
     
     barang.gudang = [gudang._id]
-    gudang.list_barang.append((barang._id, jumlah))
+    gudang.list_barang.append((barang._id, qty))
 
     document = {
         "_id": barang._id,
         "name": barang.name,
         "capacity": barang.capacity,
-        "quantity": barang.quantity,
         "description": barang.description,
         "gudang": barang.gudang
     }
     result = barang_collection.insert_one(document)
     gudang_collection.update_one({"_id": gudang._id}, {"$set": {
-        "list_barang": gudang.list_barang
+        "list_barang": gudang.list_barang,
+        "capacity" : calculate_current_capacity(gudang)
     }})
     return result.inserted_id
 
@@ -105,14 +104,53 @@ def get_all_barang() -> List[Barang]:
     return [barang_from_mongo(b) for b in barang]
 
 def update_barang(barang: Barang) -> None:
+    previous_capacity = barang_collection.find_one({"_id": barang._id})["capacity"]
     barang_collection.update_one({"_id": barang._id}, {"$set": {
         "name": barang.name,
         "capacity": barang.capacity,
-        "quantity": barang.quantity,
         "description": barang.description,
         "gudang": barang.gudang
     }})
+    for gudang_id in barang.gudang:
+        gudang = gudang_from_mongo(gudang_collection.find_one({"_id": gudang_id}))
+
+        if calculate_current_capacity(gudang) > gudang.max_capacity:
+            print("Gudang capacity exceeded")
+            barang_collection.update_one({"_id": barang._id}, {"$set": {
+                "capacity": previous_capacity
+            }})
+            return
+        
+        gudang_collection.update_one({"_id": gudang_id}, {"$set": {
+            "capacity" : calculate_current_capacity(gudang),
+            "list_barang": gudang.list_barang
+        }})
+        print(f"Gudang with ID {gudang_id} updated successfully")
     print(f"Barang with ID {barang._id} and name {barang.name} updated successfully")
+
+def update_barang_qty(barang: Barang, gudang: Gudang, qty: int) -> None:
+    for i in range(len(gudang.list_barang)):
+        if gudang.list_barang[i][0] == barang._id:
+            if qty <= 0:
+                gudang.list_barang.pop(i)
+                barang.gudang.remove(gudang._id)
+                barang_collection.update_one({"_id": barang._id}, {"$set": {
+                    "gudang": barang.gudang
+                }})
+                
+            else:
+                gudang.list_barang[i] = (barang._id, qty)
+            break
+    if calculate_current_capacity(gudang) > gudang.max_capacity:
+        print("Gudang capacity exceeded")
+        return
+    gudang_collection.update_one({"_id": gudang._id}, {"$set": {
+        "list_barang": gudang.list_barang,
+        "capacity" : calculate_current_capacity(gudang)
+    }})
+    print(f"Barang with ID {barang._id} updated successfully")
+
+    
 
 def delete_barang(_id: int) -> None:
     barang = barang_collection.find_one({"_id": _id})
@@ -120,8 +158,9 @@ def delete_barang(_id: int) -> None:
         print("Barang not found")
         return
     for gudang_id in barang["gudang"]:
-        gudang = gudang_collection.find_one({"_id": gudang_id})
+        gudang = gudang_from_mongo(gudang_collection.find_one({"_id": gudang_id}))
         gudang.list_barang = [b for b in gudang.list_barang if b[0] != _id]
+        print(f"Barang with ID {_id} deleted from Gudang with ID {gudang_id}")
 
         gudang_collection.update_one({"_id": gudang_id}, {"$set": {
             "list_barang": gudang.list_barang,
@@ -146,8 +185,18 @@ def remove_barang(barang: Barang, gudang: Gudang) -> None:
 
 # CRUD for Gudang
 def calculate_current_capacity(gudang: Gudang) -> int:
-    return sum([b[0].capacity * b[1] for b in gudang.list_barang])
+    # if gudang.list_barang is None:
+    #     return 0
+    # return sum([get_barang(b[0]).capacity * b[1] for b in gudang.list_barang])
+    sum = 0
+    for barang_id, qty in gudang.list_barang:
+        barang = get_barang(barang_id)
+        if(barang is None):
+            continue
+        sum += barang.capacity * qty
 
+    return sum
+    
 def create_gudang(gudang: Gudang) -> int:
     highest_gudang = gudang_collection.find_one({}, sort=[("_id", -1)])
 
@@ -189,6 +238,9 @@ def update_gudang(gudang: Gudang) -> int:
     if calculate_current_capacity(gudang) > gudang.max_capacity:
         print("Gudang capacity exceeded")
         return -1
+    
+    gudang_capacity = calculate_current_capacity(gudang)
+    
     gudang_collection.update_one({"_id": gudang._id}, {"$set": {
         "gudang_name": gudang.gudang_name,
         "capacity": gudang.capacity,
@@ -215,6 +267,8 @@ def delete_gudang(_id: int) -> None:
 # CRUD for Riwayat
 # ga yakin riwayat ini bener
 def create_riwayat(Riwayat) -> None:
+    highest_riwayat = riwayat_collection.find_one({}, sort=[("_id", -1)])
+    Riwayat._id = 1 if highest_riwayat is None else highest_riwayat["_id"] + 1
     document = {
         "_id": Riwayat._id,
         "value": Riwayat.value,
@@ -225,15 +279,24 @@ def create_riwayat(Riwayat) -> None:
     result = riwayat_collection.insert_one(document)
     print(f"Riwayat with ID {Riwayat._id} created successfully")
 
-# # TESTING
-# gudang = Gudang("Gudang A", 0, 1000, [])
-# create_gudang(gudang)
-# gudang = Gudang("Gudang B", 0, 1000, [])
-# create_gudang(gudang)
-# barang = Barang("Barang A", 10, 100, "Barang A description", [])
-# create_barang(barang, get_gudang(1), 10)
-# barang = Barang("Barang B", 20, 200, "Barang B description", [])
-# create_barang(barang, get_gudang(2), 20)
-# barang = Barang("Barang C", 30, 300, "Barang C description", [])
-# create_barang(barang, get_gudang(1), 30)
-# create_riwayat(Riwayat([1, 10], "ADD", "2021-08-01 12:00:00", True))
+# TESTING
+gudang1 = Gudang("Gudang 1", 0, 1000, [])
+gudang2 = Gudang("Gudang 2", 0, 1000, [])
+create_gudang(gudang1)
+create_gudang(gudang2)
+barang1 = Barang("Barang 1", 10, "Barang pertama", [])
+barang2 = Barang("Barang 2", 20, "Barang kedua", [])
+tempgudang = get_gudang(1)
+create_barang(barang1, tempgudang, 10)
+tempgudang = get_gudang(2)
+create_barang(barang2, tempgudang, 20)
+tempbarang = get_barang(1)
+tempgudang = get_gudang(1)
+update_barang_qty(tempbarang, tempgudang, 0)
+tempbarang = get_barang(1)
+tempbarang.name = "Barang 1 Updated"
+update_barang(tempbarang)
+barang3 = Barang("Barang 3", 30, "Barang ketiga", [])
+create_barang(barang3, tempgudang, 30)
+riwayat = Riwayat(1, "CREATE", "2021-08-01", True)
+create_riwayat(riwayat)
--- backend/barang/barangManager.py
+++ backend/barang/barangManager.py
@@ -1,10 +1,9 @@
 from typing import List
 
 class Barang:
-    def __init__(self, name: str, capacity: int, quantity: int, description: str, gudang: List[int]):
+    def __init__(self, name: str, capacity: int, description: str, gudang: List[int]):
         self._id = 0
         self.name = name
         self.capacity = capacity
-        self.quantity = quantity
         self.description = description
         self.gudang = gudang
