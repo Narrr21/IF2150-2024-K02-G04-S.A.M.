580a3bda6a6af5415e09eb1468c552c1fa8f35f7
Nadhif <nadhifalrozin@gmail.com>
2024-12-07 14:37:19 +0700

    feat: add save barang

--- backend/barang/barangManager.py
+++ backend/barang/barangManager.py
@@ -1,4 +1,6 @@
 from barang import Barang
+from riwayat.riwayatManager import create_riwayat_loc
+from typing import List
 from app import *
 
 class BarangManager:
@@ -21,4 +23,17 @@ class BarangManager:
     def getByName(name: str):
         get_barang_by_name(name)
     
+    def __saveHistory(act: str, value: List, success: bool):
+        if not success:
+            create_riwayat_loc(value, act, False)
+            return
+        if act == "CB":
+            create_riwayat_loc(value, act, True)
+        elif act == "DB":
+            create_riwayat_loc(value, act, True)
+        elif act == "UB":
+            create_riwayat_loc(value, act, True)
+        elif act == "PB":
+            create_riwayat_loc(value, act, True)
+            
     # def moveBarang(id_barang:int, id_gudang:int):
