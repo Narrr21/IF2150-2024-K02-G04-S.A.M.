9e6a70608a377db4e954f0e2593b9bd99154bce3
Julius <juliusarthur2005@gmail.com>
2024-12-07 14:25:00 +0700

    initial commit

--- backend/barang/barangManager.py
+++ backend/barang/barangManager.py
@@ -1,34 +1,5 @@
-from typing import List
+from barang import Barang
 from app import *
-class Barang:
-    def __init__(self, name: str, capacity: int, description: str, gudang: List[int]):
-        self._id = 0
-        self.name = name
-        self.capacity = capacity
-        self.description = description
-        self.gudang = gudang
-
-    def getID(self) -> int:
-        return self._id
-    
-    def getName(self) -> str:
-        return self.name
-    
-    def setName(self, new_name: str):
-        self.name = new_name
-    
-    def getDeskripsi(self) -> str:
-        return self.description
-    
-    def setDeskripsi(self, desc: str):
-        self.description = desc
-
-    def getCapacity(self) -> int:
-        return self.capacity
-    
-    def setCapacity(self, cap: int):
-        self.capacity = cap
-
 
 class BarangManager:
     def __init__(self):
@@ -51,4 +22,3 @@ class BarangManager:
         get_barang_by_name(name)
     
     # def moveBarang(id_barang:int, id_gudang:int):
-        
