83f261d3cc5cb7935c8ccbf3ff65b1003a064081
Julius <juliusarthur2005@gmail.com>
2024-12-07 14:17:18 +0700

    add barang manager class

--- backend/barang/barangManager.py
+++ backend/barang/barangManager.py
@@ -1,5 +1,5 @@
 from typing import List
-
+from app import *
 class Barang:
     def __init__(self, name: str, capacity: int, description: str, gudang: List[int]):
         self._id = 0
@@ -7,3 +7,48 @@ class Barang:
         self.capacity = capacity
         self.description = description
         self.gudang = gudang
+
+    def getID(self) -> int:
+        return self._id
+    
+    def getName(self) -> str:
+        return self.name
+    
+    def setName(self, new_name: str):
+        self.name = new_name
+    
+    def getDeskripsi(self) -> str:
+        return self.description
+    
+    def setDeskripsi(self, desc: str):
+        self.description = desc
+
+    def getCapacity(self) -> int:
+        return self.capacity
+    
+    def setCapacity(self, cap: int):
+        self.capacity = cap
+
+
+class BarangManager:
+    def __init__(self):
+        pass
+
+    def changeDeskripsi(id: int, desc:str):
+        target = get_barang(id)
+        target.setDeskripsi(desc)
+        update_barang(target)
+
+    def changeCapacity(id: int, cap: int):
+        target = get_barang(id)
+        target.setCapacity(cap)
+        update_barang(target)
+    
+    def getByID(id: int):
+        get_barang(id)
+
+    def getByName(name: str):
+        get_barang_by_name(name)
+    
+    # def moveBarang(id_barang:int, id_gudang:int):
+        
