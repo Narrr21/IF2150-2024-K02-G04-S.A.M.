6a1c594df5f881e964263e91506d78ca5a93b7a2
soezyxstt <16922204@mahasiswa.itb.ac.id>
2024-12-14 18:52:39 +0700

    clean up main

--- frontend/main.py
+++ frontend/main.py
@@ -7,26 +7,13 @@ from frontend.view.loginPage import LoginPage
 from frontend.view.riwayatPage import riwayatPage
 from frontend.view.gudangPage import gudangPage, moveBarangOverlay, removeBarangOverlay
 
-
-
 def main(page: ft.Page):
     # Initialize page with template
     page.__class__ = TemplatePage
     page.title = "Storage Allocation Manager"
     page.window.center()
     
-    # def show_gudang_page():
-    #     page.clean()
-    #     gudangPage(page)
-    #     page.update()
-
-    # def show_barang_page(id: int):
-    #     page.clean()
-    #     barangPage(page, id)
-    #     page.update()
-    
     def handle_login(username, password, isLoggedIn, message):
-        # TODO: Implement actual login logic
         print(f"Login attempt - Username: {username}, Password: {password}")
         # For now, just switch to main content
         if (isLoggedIn):
@@ -42,17 +29,8 @@ def main(page: ft.Page):
             content=message
         )
         page.show_dialog(dialog)
-                
-    def show_dialog(e):
-        dialog = TemplateDialog(
-            title="Sample Dialog",
-            content="This is a sample dialog with custom styling."
-        )
-        page.show_dialog(dialog)
 
-    # Pages
     login_page = LoginPage(page, handle_login)
-    # riwayat_page = riwayatPage(page)
     
     def on_page_resize(e):
         login_page.height = page.window.height
@@ -61,14 +39,12 @@ def main(page: ft.Page):
     page.on_resized = on_page_resize
 
     def show_main_content():
-        # Your existing main content setup
         content_area = ft.Container(expand=True)
         gudang_page = gudangPage(page, content_area)
 
         content_area.content = gudang_page
         
         def route_change(route):
-            # selected_index = page.navigation_rail.selected_index
             selected_index = route.control.selected_index
             content_area.clean()
             
@@ -130,10 +106,7 @@ def main(page: ft.Page):
             ], expand=True)
         )
 
-    # Start with login page
-    # barang_page = barangPage(page, 1)
     page.add(login_page)
-    # page.add(barang_page)
 
 if __name__ == "__main__":
     ft.app(target=main) 
